c52d3ecfa230dd6065b075bdbc1a02c386b81318
v.alexeev <4ff7yyry55wv6gycxoqnfu2lwd7sbstk2l5u6kgshlrylsyu3fpa>
2020-07-03 14:26:54 +0300

    secure changes for heroku

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,19 +1,20 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const config = require("dotenv").config();
-const {
-  parsed: { BACKEND_URL }
-} = config;
+require("dotenv").config();
+const { BACKEND_URL } = process.env;
 
 module.exports = function(app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: BACKEND_URL
+      target: BACKEND_URL,
+      changeOrigin: true,
+      secure: false
     })
   );
   const wsProxy = createProxyMiddleware("/socket.io", {
     target: BACKEND_URL,
     changeOrigin: true,
+    secure: false,
     ws: true
   });
   app.use(wsProxy);
